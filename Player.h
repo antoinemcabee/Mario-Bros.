ab6700fff9a37892b387a56da051708a8bf9b1f2
stephandrew <stephandrew@users.noreply.github.com>
2018-11-28 15:04:53 -0600

    Add files via upload

--- Player.h
+++ Player.h
@@ -56,9 +56,13 @@ public:
 
     }
 
+    void fallObject(){
+        posY++;
+    }
+
     void blockPlayer(int xOffset, int yOffset) {
-        posX1 = min (max (posX1 + xOffset, 0), windowWidth - width);
-        posY1 = min (max (posY1 + yOffset, 200), windowHeight - height);
+//        posX1 = min (max (posX1 + xOffset, 0), windowWidth - width);
+//        posY1 = min (max (posY1 + yOffset, 200), windowHeight - height);
 
         posX = min(max(posX + xOffset, 300), 1000 - width);
         posY = min(max(posY + yOffset, 0), 500 - height);
