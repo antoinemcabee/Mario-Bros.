10342c306c2692c73be36893cd9320b709793e6c
stephandrew <stephandrew@users.noreply.github.com>
2018-11-27 20:38:29 -0600

    Add files via upload

--- turtles.h
+++ turtles.h
@@ -12,18 +12,17 @@ using namespace std;
 
 class Turtle
 {
-private:
-    // Rectangle Variables
-    int width = 20;
-    int height = 35;
-    int posX = 980;
-    int posY = 180;
-    int posX1 = 0;
-    int posY1 = 200;
-
 public:
+        int width = 20;
+        int height = 20;
+        int posX = 980;
+        int posY = 170;
+        int posX1 = 0;
+        int posY1 = 200;
+
     void moveOpponent(int xOffset, int yOffset)
     {
+
         posX = min (max (posX + xOffset, 0), windowWidth - width);
         posY = min (max (posY + yOffset, 0), windowHeight - height);
 
@@ -45,6 +44,33 @@ public:
         }
 
     }
+
+
+  void drawOpponent1(SDL_Plotter & g)
+    {
+
+        int width = 20;
+        int height = 20;
+        int posY = 370;
+        int posX1 = 0;
+        int posY1 = 200;
+
+        // Draw Loop
+        for (int col = posX; col < width + posX; ++col)
+        {
+            for (int row = posY; row < height + posY; ++row)
+            {
+                g.plotPixel(col, row, 200, 0, 0);
+            }
+
+        }
+
+    }
+
+    void moveOpponentLeft(){
+        posX--;
+    }
+
 
 };
 
